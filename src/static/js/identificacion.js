917eafbfdf9d5b7f3cc273c22f4ddb706f96c92d
Hannia-laboo <hannialaboo3@gmail.com>
2026-07-28 00:50:19 -0600

    se agrego sidebar y sigue en construccion las demas funciones, ya funciona boton de inicio

--- src/static/js/identificacion.js
+++ src/static/js/identificacion.js
@@ -1,20 +1,29 @@
-window.onload = function(){
-    setTimeout(()=>{
-        document.getElementById("pantallaCarga").style.opacity="0";
-        setTimeout(()=>{
-            document.getElementById("pantallaCarga").style.display="none";
-            document.getElementById("contenido").style.display="block";
-        },800);
-    },2000);
+window.onload = function () {
+    setTimeout(() => {
+        document.getElementById("pantallaCarga").style.opacity = "0";
+        setTimeout(() => {
+            document.getElementById("pantallaCarga").style.display = "none";
+            document.getElementById("contenido").style.display = "block";
+        }, 800);
+    }, 2000);
 }
 
 document.querySelectorAll(".btn-principal").forEach(boton => {
-    boton.addEventListener("click", ()=>{
-        if(boton.dataset.url){
+    boton.addEventListener("click", () => {
+        if (boton.dataset.url) {
             window.location.href = boton.database.url;
-        }    
+        }
     });
-});        
+});
+
+const menuToggle = document.getElementById("menuToggle");
+const sidebar = document.getElementById("sidebar");
+
+if (menuToggle && sidebar) {
+    menuToggle.addEventListener("click", () => {
+        sidebar.classList.toggle("open");
+    });
+}
 
 //clase pendiente de estudiar y completar
 let respuestas = {
